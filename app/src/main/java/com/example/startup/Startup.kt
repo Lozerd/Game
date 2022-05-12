af5a4bd75357b935534b193d377da8a7716b10c0
Lozerd <dmisha29411@gmail.com>
2022-05-12 18:41:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/startup/Startup.kt
@@ -0,0 +1,12 @@
+package com.example.startup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.game.R
+
+class Startup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
